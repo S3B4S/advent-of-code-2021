b867213f2b0a7f369e463db4b5a8d2ae4fbf9187
Kevin van der Werff <Kevinsebastianate@hotmail.com>
2021-12-15 19:06:08 +0100

    solve day 2

--- day-02/main.hs
+++ day-02/main.hs
@@ -1,4 +1,6 @@
 import Data.List
+import Control.Arrow ((&&&))
+import Data.Bifunctor (bimap)
 
 data Direction = Up Int | Down Int | Forward Int
 
@@ -20,9 +22,22 @@ amendCoordinates (Down n) (x, y) = (x, y + n)
 calculateCoordinatesP1 :: [Direction] -> (Int, Int)
 calculateCoordinatesP1 = foldl (flip amendCoordinates) (0, 0)
 
+amendCoordinatesWithAim :: Direction -> (Int, Int, Int) -> (Int, Int, Int)
+amendCoordinatesWithAim (Forward n) (x, y, aim) = (x + n, y + aim * n, aim)
+amendCoordinatesWithAim (Up n) (x, y, aim) = (x, y, aim - n)
+amendCoordinatesWithAim (Down n) (x, y, aim) = (x, y, aim + n)
+
 -- This involes an aim, the degree of the aim altered by up and down
-calculateCoordinatesP2 :: [Direction] -> (Int, Int)
+calculateCoordinatesP2 :: [Direction] -> (Int, Int, Int)
+calculateCoordinatesP2 = foldl (flip amendCoordinatesWithAim) (0, 0, 0) -- (X, Y, currentaim)
+
+tripleToDouble :: (a, b, c) -> (a, b)
+tripleToDouble (x, y, z) = (x, y)
+
+mapTuple :: (a -> b) -> (a, a) -> (b, b)
+mapTuple f (x, y) = (f x, f y)
 
 main :: IO ()
-main = print . uncurry (*) . calculateCoordinatesP1 . map lineToDirection . lines =<< readFile "input.txt"
+main = print . mapTuple (uncurry (*)) . (calculateCoordinatesP1 &&& tripleToDouble . calculateCoordinatesP2) . map lineToDirection . lines =<< readFile "input.txt"
 -- Part 1 solution: 1660158
+-- Part 2 solution: 1604592846
