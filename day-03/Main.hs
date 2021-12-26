d8da2feae07c8517ae42ea5dd5c5f814c9c4da20
Kevin van der Werff <Kevinsebastianate@hotmail.com>
2021-12-26 23:40:13 +0100

    day 3 part 2

--- day-03/Main.hs
+++ day-03/Main.hs
@@ -1,5 +1,6 @@
 import Debug.Trace
 import Data.Char (digitToInt)
+import Data.List (find)
 
 data Count = Count [Int] Int deriving (Show) -- First is the count, second is the total entries
 
@@ -26,6 +27,48 @@ countOnes count bits = map (uncurry (+)) $ zip count $ map digitToInt bits
 mapTuple :: (a -> b) -> (a, a) -> (b, b)
 mapTuple f (x, y) = (f x, f y)
 
-main :: IO ()
-main = print . uncurry (*) . mapTuple (binaryToDecimal (-1)) . (\x -> (invertBinary x, x) ). calculateGammaRateBinary . (\lines -> Count (foldl countOnes [] lines) (length lines)) . lines =<< readFile "input.txt"
+-- Part 1
+-- main :: IO ()
+-- main = print
+--   . uncurry (*)
+--   . mapTuple (binaryToDecimal (-1))
+--   . (\x -> (invertBinary x, x)) 
+--   . calculateGammaRateBinary
+--   . (\lines -> Count (foldl countOnes [] lines) (length lines))
+--   . lines =<< readFile "input.txt"
 -- Part 1 solution: 3885894
+
+-- Part 2
+invertBit :: Int -> Int
+invertBit 1 = 0
+invertBit 0 = 1
+invertBit _ = -1
+
+charToInt :: Char -> Int
+charToInt '1' = 1
+charToInt '0' = 0
+
+data System = Oxygen | CO2
+
+count :: Eq a => (a -> Bool) -> [a] -> Int
+count f = length . filter f
+
+determineBitCriteria :: System -> [(String, String)] -> Char
+determineBitCriteria Oxygen bitSequences = if amountBitCriteria >= (length bitSequences - amountBitCriteria) then '1' else '0'
+  where amountBitCriteria = count ((== '1') . head . fst) bitSequences
+determineBitCriteria CO2 bitSequences = if amountBitCriteria <= (length bitSequences - amountBitCriteria) then '0' else '1'
+  where amountBitCriteria = count ((== '0') . head . fst) bitSequences
+
+filterBitCriteria :: System -> [(String, String)] -> [(String, String)]
+filterBitCriteria system bitSequences = (map (\(f, s) -> (tail f, s)) . filter ((== bitCriteria) . head . fst)) bitSequences
+  where bitCriteria = determineBitCriteria system bitSequences
+
+main :: IO ()
+main = print
+  . (\(Just x, Just y) -> binaryToDecimal (-1) x * binaryToDecimal (-1) y)
+  . (\x -> (
+      (fmap (snd . head) . find ((== 1) . length) . iterate (filterBitCriteria Oxygen)) x,
+      (fmap (snd . head) . find ((== 1) . length) . iterate (filterBitCriteria CO2)) x)
+    )
+  . map (\x -> (x, x))
+  . lines =<< readFile "input.txt"
